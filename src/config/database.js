aac4ae4f0b1ef53c5d437bd59b210b85f9cb33e7
asia <asiia92@hotmail.com>
2025-05-23 16:54:16 +0200

    implement docker compose + postgresql

--- src/config/database.js
+++ src/config/database.js
@@ -5,18 +5,24 @@ dotenv.config();
 
 let sequelize;
 
-if (process.env.NODE_ENV !== "test") {
-  sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: process.env.DB_PATH,
-    logging: false, // Set to console.log to see SQL queries
-  });
-} else {
+if (process.env.NODE_ENV === "test") {
   sequelize = new Sequelize({
     dialect: "sqlite",
     storage: ":memory:",
-    logging: false, // Set to console.log to see SQL queries
+    logging: false,
   });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME || "postgres",
+    process.env.DB_USER || "postgres",
+    process.env.DB_PASS || "postgres",
+    {
+      host: process.env.DB_HOST || "db",
+      dialect: "postgres",
+      port : process.env.DB_PORT || 5432,
+      logging: false,
+    }
+  );
 }
 
 module.exports = sequelize;
